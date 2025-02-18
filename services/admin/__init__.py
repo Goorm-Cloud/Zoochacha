44952f4ecf181638ad6386ba86fe1514c1b2d02d
ellen310 <ellen10735@gmail.com>
2025-02-18 14:53:34 +0900

    feat: cognito login and routing

--- services/admin/__init__.py
+++ services/admin/__init__.py
@@ -1,14 +1,13 @@
 import os
 from flask import Flask
 from .routes import admin_bp, login_bp
-from services.common import config, db, migrate
+#from services.common import config, db, migrate
 from services.common.oauth import oauth
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    app.secret_key = os.urandom(24)  # 임시 키 설정
-    #app.config.from_object(config)
+    app.secret_key = os.urandom(24)
 
 
     # OAuth 초기화
@@ -22,13 +21,13 @@ def create_app():
         client_kwargs={'scope': 'phone openid email'}
     )
 
-    #ORM
-    db.init_app(app)
-    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
-        migrate.init_app(app, db, render_as_batch=True)
-    else:
-        migrate.init_app(app, db)
-    from services.common import models
+    # #ORM
+    # db.init_app(app)
+    # if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+    #     migrate.init_app(app, db, render_as_batch=True)
+    # else:
+    #     migrate.init_app(app, db)
+    # from services.common import models
 
 
     # 블루프린트 등록
